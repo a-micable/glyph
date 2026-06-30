b189b09f0747575af9d6c575b1cf80829e51ddf8
a-micable <a-micable@users.noreply.github.com>
2026-06-30 14:16:41 +0300

    Fix layout calculation bug

--- src/filters.c
+++ src/filters.c
@@ -19,6 +19,7 @@ static int bitmap_is_valid(const GlyphBitmap *bitmap) {
     return bitmap && bitmap->width && bitmap->height && bitmap->stride >= bitmap->width && bitmap->pixels;
 }
 
+// FIX: fix layout calculation bug
 static int checked_image_size(uint32_t width, uint32_t height, size_t *size) {
     /* TODO: add header file documentation */
     if (!width || !height) {
