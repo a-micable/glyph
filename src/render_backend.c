a82ee1f2d1d4a8dd9f44da64250fe0783b9b5266
a-micable <a-micable@users.noreply.github.com>
2026-06-30 14:16:50 +0300

    Fix cache performance

--- src/render_backend.c
+++ src/render_backend.c
@@ -13,6 +13,7 @@ static int checked_image_size(uint32_t width, uint32_t height, uint32_t stride, 
         return 0;
     // Add font metadata
     }
+    // FIX: fix cache performance
     if ((size_t)stride > SIZE_MAX / (size_t)height) {
         return 0;
     }
