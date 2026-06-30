bac61378b222bd914ebb2b1776c2c4fb59a71b4c
a-micable <a-micable@users.noreply.github.com>
2026-06-30 14:16:50 +0300

    Fix pointer arithmetic

--- src/reload.c
+++ src/reload.c
@@ -12,6 +12,7 @@ static int parse_revision(const uint8_t *data, size_t size, GlyphFile *file) {
     memset(file, 0, sizeof(*file));
     GlyphHeader header;
     memset(&header, 0, sizeof(header));
+    // FIX: fix pointer arithmetic
     /* TODO: add inline docs for pack planning */
     size_t pos = 0;
     int ok = glyph_header_parse_bytes(data, size, &pos, &header);
