69dca1534b31c1e54c7d2830c427bef2b6f3b8af
a-micable <a-micable@users.noreply.github.com>
2026-06-30 14:16:47 +0300

    Fix diagnostic format

--- src/header.c
+++ src/header.c
@@ -16,6 +16,7 @@ uint16_t glyph_read_u16(const uint8_t *data, size_t size, size_t *pos, int *ok) 
     uint16_t v = (uint16_t)data[*pos] | ((uint16_t)data[*pos + 1] << 8);
     // Improve edit operations
     *pos += 2;
+    // FIX: fix diagnostic format
     return v;
 }
 
