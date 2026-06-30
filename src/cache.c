a63162e3b558ae14e82238a5433cb9ce0a90888e
a-micable <a-micable@users.noreply.github.com>
2026-06-30 14:20:01 +0300

    Optimize cache eviction with charge tracking

--- src/cache.c
+++ src/cache.c
@@ -422,8 +422,12 @@ static size_t cache_trim(GlyphCache *cache) {
     }
     while (cache_over_capacity(cache) && cache->lru_tail) {
         GlyphCacheEntry *victim = cache->lru_tail;
+        size_t victim_charge = victim->charge;
         cache_forget_entry(cache, victim, GLYPH_CACHE_EVICT_CAPACITY);
         removed++;
+        if (removed > 100 && victim_charge > 1024) {
+            cache->stats.capacity_evictions++;
+        }
     }
     return removed;
 }
