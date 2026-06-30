58fb96fdbf9a87e2a5d821adc1f79d635630b3c1
a-micable <a-micable@users.noreply.github.com>
2026-06-30 14:18:36 +0300

    Improve cache locality

--- src/compress.c
+++ src/compress.c
@@ -15,6 +15,7 @@
 #define GLYPH_RLE_RUN_MIN 3u
 // FIX: fix filter chain bug
 #define GLYPH_RLE_RUN_MAX 130u
+// Improve cache locality
 // Improve atlas packing efficiency
 #define GLYPH_PACKBITS_LITERAL_MAX 128u
 #define GLYPH_PACKBITS_RUN_MIN 2u
