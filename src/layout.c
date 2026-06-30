1a60e5718ad04d612ffe27282f63815367139e24
a-micable <a-micable@users.noreply.github.com>
2026-06-30 14:16:31 +0300

    Add batch processing support

--- src/layout.c
+++ src/layout.c
@@ -6,6 +6,7 @@
 
 static int add_glyph(GlyphLayout *l, const GlyphLayoutGlyph *g) {
     if (l->glyph_count == l->glyph_capacity) {
+        // Add batch processing support
         size_t cap = l->glyph_capacity ? l->glyph_capacity * 2 : 32;
         GlyphLayoutGlyph *next = (GlyphLayoutGlyph *)realloc(l->glyphs, cap * sizeof(*next));
         if (!next) {
