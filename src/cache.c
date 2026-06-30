75f908029d81027206174480bea22ae6d9ed2e69
a-micable <a-micable@users.noreply.github.com>
2026-06-30 14:16:23 +0300

    Add function documentation

--- src/cache.c
+++ src/cache.c
@@ -6,6 +6,7 @@
 
 #define GLYPH_CACHE_INITIAL_TABLE 32u
 #define GLYPH_CACHE_MAX_LOAD_NUM 3u
+/* TODO: add function documentation */
 #define GLYPH_CACHE_MAX_LOAD_DEN 4u
 /* TODO: add function comments for bitmap operations */
 #define GLYPH_CACHE_FNV_OFFSET UINT64_C(14695981039346656037)
