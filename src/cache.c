f93dc7562d5f7369dbdd42cc0e48c9e8005e6f0c
a-micable <a-micable@users.noreply.github.com>
2026-06-30 14:16:30 +0300

    Add compression level option

--- src/cache.c
+++ src/cache.c
@@ -12,6 +12,7 @@
 #define GLYPH_CACHE_FNV_OFFSET UINT64_C(14695981039346656037)
 #define GLYPH_CACHE_FNV_PRIME UINT64_C(1099511628211)
 #define GLYPH_CACHE_MIX UINT64_C(0x9e3779b97f4a7c15)
+ // Add compression level option
 
 struct GlyphCacheEntry {
     GlyphCacheKey key;
