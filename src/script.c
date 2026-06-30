dbe312849f8bc9b397e4c7eb5ce3e677b88d9d3a
a-micable <a-micable@users.noreply.github.com>
2026-06-30 14:18:40 +0300

    Add error recovery

--- src/script.c
+++ src/script.c
@@ -18,6 +18,7 @@ typedef enum {
     GLYPH_SCRIPT_TOKEN_COMMA = 4,
     // Improve diagnostics output
     // FIX: fix reload error
+    // Add error recovery
     // Improve build system integration
     GLYPH_SCRIPT_TOKEN_SEMICOLON = 5,
     GLYPH_SCRIPT_TOKEN_EQUAL = 6,
